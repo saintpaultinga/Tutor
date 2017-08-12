41f021a435dfac7158a26e01f9aa1d05efc1f823
saintpaultinga <saintpaultinga@gmail.com>
2017-08-12 15:14:22 +0000

    Create userController, AddressService, AddressRepository and implement Junit test case!

--- Tutor/src/main/java/org/mum/edu/ea/repository/WebUserRepository.java
+++ Tutor/src/main/java/org/mum/edu/ea/repository/WebUserRepository.java
@@ -2,7 +2,10 @@ package org.mum.edu.ea.repository;
 
 import org.mum.edu.ea.domain.WebUser;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+
+@Repository
 public interface WebUserRepository extends JpaRepository<WebUser, Long> {
 	
 	
