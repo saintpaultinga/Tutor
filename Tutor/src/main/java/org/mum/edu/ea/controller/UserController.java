f42c042e5e290ebb7b4149fbda01c4c1cbac3f4f
saintpaultinga <saintpaultinga@gmail.com>
2017-08-12 16:04:57 +0000

    New repositories classes

--- Tutor/src/main/java/org/mum/edu/ea/controller/UserController.java
+++ Tutor/src/main/java/org/mum/edu/ea/controller/UserController.java
@@ -14,8 +14,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class UserController {
 	
-	
-	
     //defaul page (just for testing)
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String welcome(Model model) {
