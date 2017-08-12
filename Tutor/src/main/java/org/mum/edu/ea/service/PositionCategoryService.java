d9de2f2743ed8ab4ddd83ef42544d160ee62e5a3
saintpaultinga <saintpaultinga@gmail.com>
2017-08-12 23:09:18 +0000

    PositionCategotyRepository added

---
+++ Tutor/src/main/java/org/mum/edu/ea/service/PositionCategoryService.java
@@ -0,0 +1,8 @@
+package org.mum.edu.ea.service;
+
+import org.mum.edu.ea.domain.PositionCategory;
+
+public interface PositionCategoryService {
+    
+	public PositionCategory FindByType(String type);
+}
